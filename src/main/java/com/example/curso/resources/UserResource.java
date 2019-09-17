591787a1f25b91ce28ea51266329976e9afb67c2
Dejanir Junior <junekko@msn.com>
2019-09-17 19:49:43 -0300

    UserDTO, findById, findAll

--- src/main/java/com/example/curso/resources/UserResource.java
+++ src/main/java/com/example/curso/resources/UserResource.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.example.curso.dto.UserDTO;
 import com.example.curso.entities.User;
 import com.example.curso.services.UserService;
 
@@ -26,16 +27,16 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll() {
-		List<User> list = service.findAll();		
+	public ResponseEntity<List<UserDTO>> findAll() {
+		List<UserDTO> list = service.findAll();		
 		return ResponseEntity.ok().body(list);
 	
 	}
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
-	User obj = service.findById(id);
-	return ResponseEntity.ok().body(obj);
+	public ResponseEntity<UserDTO> findById(@PathVariable Long id){
+	UserDTO dto = service.findById(id);
+	return ResponseEntity.ok().body(dto);
 	}
 	
 	@PostMapping
