bc61ebe4030a5e6ab97c30e13d959344d5d91dfb
Dejanir Junior <junekko@msn.com>
2019-10-01 20:22:23 -0300

    Data model

--- src/main/java/com/example/curso/config/TestConfig.java
+++ src/main/java/com/example/curso/config/TestConfig.java
@@ -13,12 +13,14 @@ import com.example.curso.entities.Order;
 import com.example.curso.entities.OrderItem;
 import com.example.curso.entities.Payment;
 import com.example.curso.entities.Product;
+import com.example.curso.entities.Role;
 import com.example.curso.entities.User;
 import com.example.curso.entities.enums.OrderStatus;
 import com.example.curso.repositories.CategoryRepository;
 import com.example.curso.repositories.OrderItemRepository;
 import com.example.curso.repositories.OrderRepository;
 import com.example.curso.repositories.ProductRepository;
+import com.example.curso.repositories.RoleRepository;
 import com.example.curso.repositories.UserRepository;
 
 @Configuration
@@ -36,10 +38,12 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
-	
-	
+		
 	@Autowired
 	private OrderItemRepository orderItemRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
 	
 	@Override
 	public void run(String... args) throws Exception {
@@ -70,11 +74,24 @@ public class TestConfig implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");	
 	
+		userRepository.saveAll(Arrays.asList(u1, u2));
+		
+		Role r1 = new Role(null, "ROLE_CLIENT");
+		Role r2 = new Role(null, "ROLE_ADMIN");
+		
+		roleRepository.saveAll(Arrays.asList(r1, r2));
+		
+		u1.getRoles().add(r1);
+		u2.getRoles().add(r1);
+		u2.getRoles().add(r2);
+		
+		userRepository.saveAll(Arrays.asList(u1, u2));
+		
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT,u1);
 		
-		userRepository.saveAll(Arrays.asList(u1, u2));
+		
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	
 		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
