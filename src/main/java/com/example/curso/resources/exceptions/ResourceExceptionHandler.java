986c3f26ea6b1884504239b63202422a7965df09
Dejanir Junior <junekko@msn.com>
2019-10-08 20:33:59 -0300

    User findById and update - validade self or admin

--- src/main/java/com/example/curso/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/example/curso/resources/exceptions/ResourceExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.example.curso.services.exceptions.DatabaseException;
 import com.example.curso.services.exceptions.JWTAuthenticationException;
+import com.example.curso.services.exceptions.JWTAuthorizationException;
 import com.example.curso.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -49,6 +50,14 @@ public class ResourceExceptionHandler {
 	public ResponseEntity<StandardError> jwtAuthentication(JWTAuthenticationException e, HttpServletRequest request) {
 		String error = "Authentication Error";
 		HttpStatus  status = HttpStatus.UNAUTHORIZED;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(JWTAuthorizationException.class)
+	public ResponseEntity<StandardError> jwtAuthorization(JWTAuthorizationException e, HttpServletRequest request) {
+		String error = "Authentication Error";
+		HttpStatus  status = HttpStatus.FORBIDDEN;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
 	}
--- src/main/java/com/example/curso/services/AuthService.java
+++ src/main/java/com/example/curso/services/AuthService.java
@@ -5,13 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.example.curso.dto.CredentialsDTO;
 import com.example.curso.dto.TokenDTO;
+import com.example.curso.entities.User;
+import com.example.curso.repositories.UserRepository;
 import com.example.curso.security.JWTUtil;
 import com.example.curso.services.exceptions.JWTAuthenticationException;
+import com.example.curso.services.exceptions.JWTAuthorizationException;
 
 @Service
 public class AuthService {
@@ -21,6 +26,9 @@ public class AuthService {
 	
 	@Autowired
 	private JWTUtil jwtUtil;
+	
+	@Autowired
+	private UserRepository  userRepository;
 	
 	@Transactional(readOnly = true)
 	public TokenDTO authenticate(CredentialsDTO dto) {
@@ -34,4 +42,20 @@ public class AuthService {
 		}
 	}
 	
+	public User authenticated() {
+		try {
+			UserDetails userDetails = (UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+			return userRepository.findByEmail(userDetails.getUsername());
+		} catch (Exception e) {
+			throw new JWTAuthorizationException("Access denied");
+		}
+		
+	}
+	
+	public void validateSelfOrAdmin(Long userId) {
+		User user = authenticated();
+		if (user == null || (!user.getId().equals(userId)) && !user.hasRole("ROLE_ADMIN")) {
+			throw new JWTAuthorizationException("Access denied");
+		}
+	}
 }
